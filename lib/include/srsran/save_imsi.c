a2c13a57b71bae708814d3bd45d9c5e4d47cd912
c3t4r4 <neocetara@hotmail.com>
2022-04-20 10:08:17 -0300

    First Commit

---
+++ lib/include/srsran/save_imsi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <float.h>
+#include <complex.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <inttypes.h> // for printing uint64_t
+#include "srsran/phy/utils/vector.h"
+
+char * gettime() {
+    time_t mytime = time(NULL);
+    char * time_str = ctime( & mytime);
+    time_str[strlen(time_str) - 1] = '\0';
+    return time_str;
+}
+
+void write_IMSI(FILE * output, uint64_t payload) {
+    fprintf(output, "%s; IMSI: %015" PRIu64 " \n", gettime(), payload);
+}
+
+void save_imsi(char * file_imsi, uint64_t payload) {
+    FILE * fp;
+    fp = fopen(file_imsi, "a");
+
+    if (fp) {
+        write_IMSI(fp, payload);
+        fclose(fp);
+    } else {
+        perror("fopen");
+    }
+}
