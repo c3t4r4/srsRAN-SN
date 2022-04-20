91ff9507d76ea7866fdf30117da68879e46a5580
c3t4r4 <neocetara@hotmail.com>
2022-04-20 13:46:53 -0300

    Add Sqlite

--- lib/include/srsran/save_imsi.c
+++ lib/include/srsran/save_imsi.c
@@ -8,7 +8,15 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <inttypes.h> // for printing uint64_t
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <string>
 #include "srsran/phy/utils/vector.h"
+#include "sqlite/sqlite3.h"
+
+sqlite3* db;
+sqlite3_stmt * stmt;
 
 char * gettime() {
     time_t mytime = time(NULL);
@@ -17,9 +25,31 @@ char * gettime() {
     return time_str;
 }
 
-void write_IMSI(FILE * output, uint64_t payload) {
-    fprintf(output, "%s; IMSI: %015" PRIu64 " \n", gettime(), payload);
+char * gettimeDB() {
+    time_t mytime = time(NULL);
+    struct tm * timeinfo;
+
+    char timestr[30];
+    char buffer [30];
+
+    time (&mytime);
+    timeinfo = localtime (&mytime);
+
+    strftime(timestr, sizeof(timestr), "%Y-%m-%d %X", timeinfo);
+    sprintf(buffer,"%s", timestr);
+    string stdBuffer(buffer);
+    return stdBuffer;
 }
+
+void write_IMSI(FILE * output, uint64_t payload) {
+    fprintf(output, "%s; IMSI: %015" PRIu64 " \n", gettimeDB(), payload);
+}
+
+void save_IMSI_BD(){
+    sqlite3_open("imsi.db", &db);
+}
+
+
 
 void save_imsi(char * file_imsi, uint64_t payload) {
     FILE * fp;
