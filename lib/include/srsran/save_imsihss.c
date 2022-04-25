402aa581b23cecfd729cbca0af820b353c4aff89
c3t4r4 <neocetara@hotmail.com>
2022-04-25 17:42:40 -0300

    create database

--- lib/include/srsran/save_imsihss.c
+++ lib/include/srsran/save_imsihss.c
@@ -9,7 +9,13 @@
 #include <ctype.h>
 #include <inttypes.h> // for printing uint64_t
 #include <stdlib.h>
+#include <iostream.h>
+#include <sqlite3.h>
 #include "srsran/phy/utils/vector.h"
+
+char *dbFile = (char *)"/home/epcIMSI.db";
+
+
 
 // char * gettimeOld() {
 //     time_t mytime = time(NULL);
@@ -17,6 +23,24 @@
 //     time_str[strlen(time_str) - 1] = '\0';
 //     return time_str;
 // }
+
+void createTable(sqlite3* DB){
+    std::string sql =   "CREATE TABLE IF NOT EXISTS IMSI("
+                        "ID INT PRIMARY KEY NOTNULL, "
+                        "IMSI TEXT NOTNULL, "
+                        "DATE TEXT NOTNULL, "
+                        "TYPE TEXT NOTNULL, "
+                        "UPDATE INT NOTNULL); ";
+    int sqlStatus = 0;
+    char* messageError;
+    sqlStatus = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+    if(sqlStatus ≃ SQLITE_OK){
+        std::cerr << "Error Create Table" << std::endl;
+        sqlite3_free(messageError);
+    }else{
+        std::cout << "Table create Successfully" << std::endl;
+    }
+}
 
 std::string gettimeBDhss() {
     std::time_t now = std::time(NULL);
@@ -35,12 +59,20 @@ void write_IMSI_hss(FILE * output, uint64_t payload, std::string type) {
 
 void save_imsi_hss(char * file_imsi, uint64_t payload, std::string type) {
     FILE * fp;
-    // if((file = fopen(file_imsi,"r"))!=NULL)
-    // {
-    //     fclose(file);
-    //     rename(file_imsi, "readme.txt");
-    // }
     fp = fopen(file_imsi, "a");
+
+    sqlite3* DB;
+    int dbstatus = 0;
+    dbstatus = sqlite3_open(dbFile, &DB);
+    
+    if(dbstatus){
+        std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+        sqlite3_create_filename()
+    }
+
+    createTable(DB);
+
+
 
     if (fp) {
         write_IMSI_hss(fp, payload, type);
