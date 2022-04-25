b0c28843ca8f1e6c34b5f17fec73d1213d14a23e
c3t4r4 <neocetara@hotmail.com>
2022-04-25 18:25:36 -0300

    Fix

--- lib/include/srsran/save_imsihss.c
+++ lib/include/srsran/save_imsihss.c
@@ -86,7 +86,8 @@ void save_imsi_hss(char * file_imsi, uint64_t payload, std::string type) {
 
     std::string imsi = std::to_string(payload);
 
-    std::string sql = "INSERT INTO IMSI (IMSI,DATE,TYPE,UPDATE) VALUES ('" + std::to_string(payload) + "','" + gettimeBDhss().c_str() + "','" + type.c_str() + "',0);";
+    sql = "INSERT INTO IMSI (IMSI,DATE,TYPE,UPDATE) VALUES ('" + std::to_string(payload) + "','" + gettimeBDhss().c_str() + "','" + type.c_str() + "',0);";
+    
     dbstatus = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
     if (dbstatus != SQLITE_OK) {
         std::cerr << "Error Insert" << std::endl;
